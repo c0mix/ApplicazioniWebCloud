480f0ee5d5b73157cca42f6da4a945da508d718d
c0mix <lorenzo.comi@studenti.unimi.it>
2017-06-29 19:06:20 +0200

    updateFinal

---
+++ client/italiaSport.js
@@ -0,0 +1,156 @@
+var sportiviDat = [{
+	id:"0001",
+	name: "Roberto",	
+	lastname:"Baggio",
+	sport:"calcio",
+	birth:{
+		placeOfBirth:"Caldogno",
+		country:"USA",
+		date:"18/02/1967"
+	},
+	height:174,
+	weight:73,
+	thumbnail:"https://assets.metrolatam.com/do/2016/06/23/gettyimages-52920054-800x800.jpg"	
+},
+{
+	id:"0002",
+	name: "Michael",	
+	lastname:"Jordan",
+	sport:"basket",
+	birth:{
+		placeOfBirth:"Brooklyn, New York",
+		country:"USA",
+		date:"17/02/1963"
+	},
+	height:198,
+	weight:101,
+	thumbnail:"http://www.lorenzodamelio.it/wp-content/uploads/2016/01/MichaelJordan.png"
+},
+{
+	id:"0003",
+	name: "Andre",	
+	lastname:"Agassi",
+	sport:"tennis",
+	birth:{
+		placeOfBirth:"Las Vegas, Nevada",
+		country:"USA",
+		date:"29/04/1970"
+	},
+	height:180,
+	weight:80,
+	thumbnail:"https://www.autographs.co.uk/image/cache/catalog/autographs/andre-agassi-signed-authentic-genuine-signature-1934-800x800.jpg"
+},
+{
+	id:"0004",
+	name: "LeBron",	
+	lastname:"James",
+	sport:"basket",
+	birth:{
+		placeOfBirth:"Akron",
+		country:"USA",
+		date:"30/12/1984"
+	},
+	height:203,
+	weight:113,
+	thumbnail:"https://media.timplee.com/__sized__/products/maglia-basket-adidas-replica-lebron-james-23-854217d213334e232e5b6931c50bc23f.jpg"	
+},
+{
+	id:"0005",
+	name: "Tiger",	
+	lastname:"Woods",
+	sport:"golf",
+	birth:{
+		placeOfBirth:"Cypress",
+		country:"USA",
+		date:"30/12/1975"
+	},
+	height:185,
+	weight:70,
+	thumbnail:"https://www.hookedongolfblog.com/wp-content/uploads/2016/12/TigerWoodsBridgestone.jpg"	
+},
+{
+	id:"0006",
+	name: "Tom",	
+	lastname:"Brady",
+	sport:"football",
+	birth:{
+		placeOfBirth:"San Mateo",
+		country:"USA",
+		date:"03/08/1977"
+	},
+	height:190,
+	weight:90,
+	thumbnail:"https://www.blueweigh.net/wp-content/uploads/2017/03/Tom-Brady-800x800.jpg"	
+},
+{
+	id:"0007",
+	name: "Roger",	
+	lastname:"Federer",
+	sport:"tennis",
+	birth:{
+		placeOfBirth:"Basilea",
+		country:"Swizzera",
+		date:"08/08/1981"
+	},
+	height:185,
+	weight:70,
+	thumbnail:"https://s-media-cache-ak0.pinimg.com/originals/01/47/9d/01479d0bc517c73d74d00a9952606e7d.jpg"	
+},
+{
+	id:"0008",
+	name: "Michael",	
+	lastname:"Shumacher",
+	sport:"automobilismo",
+	birth:{
+		placeOfBirth:"Hurt",
+		country:"Germania",
+		date:"03/01/1969"
+	},
+	height:174,
+	weight:75,
+	thumbnail:"http://3.citynews-today.stgy.ovh/~media/original-hi/17473543913405/michael_schumacher.jpg"	
+},
+{
+	id:"0009",
+	name: "Usain",	
+	lastname:"Bolt",
+	sport:"atletica",
+	birth:{
+		placeOfBirth:"Sherwood",
+		country:"Giamaica",
+		date:"21/08/1986"
+	},
+	height:195,
+	weight:85,
+	thumbnail:"https://ae01.alicdn.com/kf/HTB1vZRGPVXXXXadXVXXq6xXFXXXC/J0138-font-b-Usain-b-font-font-b-Bolt-b-font-Athletes-Pop-14x21-24x36-Inches.jpg"	
+},
+{
+	id:"0010",
+	name: "Myke",	
+	lastname:"Tison",
+	sport:"boxe",
+	birth:{
+		placeOfBirth:"Bedfort",
+		country:"USA",
+		date:"30/06/1966"
+	},
+	height:178,
+	weight:109,
+	thumbnail:"https://turntable.kagiso.io/images/mike_tyson_bio.width-800.jpg"	
+},
+{
+	id:"0011",
+	name: "Cristiano",	
+	lastname:"Ronaldo",
+	sport:"calcio",
+	birth:{
+		placeOfBirth:"Functhal",
+		country:"Portogallo",
+		date:"05/02/1985"
+	},
+	height:185,
+	weight:80,
+	thumbnail:"http://static.dagospia.com/img/foto/01-2016/cristiano-ronaldo-758164.jpg"	
+}
+]
+
